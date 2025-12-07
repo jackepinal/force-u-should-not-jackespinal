b47b91028b6201b4a3685ca429818d981802afa4
Jack Espinal <jespinal@lion.lmu.edu>
2025-12-07 14:25:02 -0800

    assertions for list

---
+++ assertions.py
@@ -0,0 +1,6 @@
+#print list assertions
+# Below is our test code. Do not modify it, but you may add your own prints.
+assert(is_permutation([1, 2, 3], [3, 1, 2]) == True)
+assert(is_permutation([1, 1, 1, 2],[1, 2, 1, 1]) == True)
+assert(is_permutation([1, 2, 3, 1], [1, 2, 3]) == False)
+assert(is_permutation([1, 1, 2, 3], [1, 3, 2, 2]) == False)
