c01ac9c7cc040f4a9c6f8bda714f0d9a373c64bb
Jack Espinal <jespinal@lion.lmu.edu>
2025-12-07 14:08:59 -0800

    adding namem and hello

---
+++ hello.py
@@ -0,0 +1,7 @@
+# TODO: Write the same program as you wrote in the terminal for Hello World!!!
+# You can write comments by putting the symbol at the start of the line to turn the entire line into a comment
+"""
+You can write comments on multiple lines by placing the text between the lines of quotation marks. This cell does nothing now until you write some code below...
+"""
+
+print("Hello World")
